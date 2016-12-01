859b9f24bfa9c83df9d9a4ffffc4cd53abbb211c
rafaelmtn <rafaelcapitanio@hotmail.com>
2016-12-01 21:21:16 -0200

    escopo de serviços

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,11 +1,56 @@
 angular.module('starter.controllers', [])
 
-.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+.controller('AppCtrl', ["$scope", "$ionicModal", "$timeout", "$window", function($scope, $ionicModal, $timeout, $window) {
+  $scope.login = function(username, password) {
+      window.localStorage.setItem("username", username);
+      window.localStorage.setItem("password", password);
+  };
 
+  $scope.isLoggedIn = function() {
+      if(window.localStorage.getItem("username") !== undefined && window.localStorage.getItem("password") !== undefined) {
+          return true;
+      } else {
+          return false;
+      }
+  };
 
+  $scope.logout = function() {
+      window.localStorage.removeItem("username");
+      window.localStorage.removeItem("password");
+  };
+
+}])
+.controller('GpsgateController',function(){
+  function soap() {
+        var xmlhttp = new XMLHttpRequest();
+        xmlhttp.open('POST', 'sistema2.wolfrastreamento.com.br', true);
+
+        var xmlContent =
+          '<?xml version="1.0" encoding="utf-8"?>' +
+            '<soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">'+
+                '<soap:Body>'+
+                  '<Login xmlns="http://gpsgate.com/services/">' +
+                      '<strUsername>rafael</strUsername>' +
+                      '<strPassword>rafael123</strPassword>' +
+                      '<iApplicationID>21</iApplicationID>' +
+                    '</Login>'+
+                '</soap:Body>'+
+            '</soap:Envelope>';
+
+         xmlhttp.onreadystatechange = function () {
+             if (xmlhttp.readyState == 4) {
+                 if (xmlhttp.status == 200) {
+                     console.log('ok');
+                 }
+             }
+         }
+
+         xmlhttp.setRequestHeader('Content-Type', 'text/xml');
+         xmlhttp.send(xmlContent);
+         console.log(xmlhttp);
+     }
 })
-
-.controller('MapCtrl', function($scope,$ionicLoading) {
+.controller('MapCtrl', ["$scope","$ionicLoading", function($scope,$ionicLoading) {
     this.initMap = function () {
       google.maps.event.addDomListener(window, 'load', function() {
           var myLatlng = new google.maps.LatLng(37.3000, -120.4833);
@@ -31,7 +76,7 @@ angular.module('starter.controllers', [])
           $scope.map = map;
       });
     };
-})
+}])
 .controller('cadastrarCtrl', ['$scope', '$state',function ($scope, $state) {
 
 
